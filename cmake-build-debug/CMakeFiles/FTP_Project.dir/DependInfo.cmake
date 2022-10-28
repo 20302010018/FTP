49e176f1c38060aa12bcf90abd54ee3956c57cb3
biyuyang <1600224799@qq.com>
2022-10-28 20:10:41 +0800

    change makefile

---
+++ cmake-build-debug/CMakeFiles/FTP_Project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/coding/computer_network/FTP/source/FTPClient/client.c" "CMakeFiles/FTP_Project.dir/source/FTPClient/client.c.obj" "gcc" "CMakeFiles/FTP_Project.dir/source/FTPClient/client.c.obj.d"
+  "D:/coding/computer_network/FTP/source/FTPServer/server.c" "CMakeFiles/FTP_Project.dir/source/FTPServer/server.c.obj" "gcc" "CMakeFiles/FTP_Project.dir/source/FTPServer/server.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FTP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FTP_Project.dir/source/FTPClient/client.c.obj"
+  "CMakeFiles/FTP_Project.dir/source/FTPClient/client.c.obj.d"
+  "CMakeFiles/FTP_Project.dir/source/FTPServer/server.c.obj"
+  "CMakeFiles/FTP_Project.dir/source/FTPServer/server.c.obj.d"
+  "FTP_Project.exe"
+  "FTP_Project.exe.manifest"
+  "FTP_Project.pdb"
+  "libFTP_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FTP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
