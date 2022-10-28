5fc5607386ed07675f6e4259e2a8d482c0d02440
biyuyang <1600224799@qq.com>
2022-10-28 20:32:35 +0800

    ?

---
+++ source/FTPServer/server.h
@@ -0,0 +1,10 @@
+//
+// Created by byy on 2022/10/28.
+//
+
+#ifndef C_SOCKET_SERVER_H
+#define C_SOCKET_SERVER_H
+
+int server();
+
+#endif //C_SOCKET_SERVER_H
---
+++ source/FTPServer/server_main.c
@@ -0,0 +1,12 @@
+//
+// Created by byy on 2022/10/28.
+//
+
+#include <stdio.h>
+#include "server.h"
+
+int main() {
+    printf("Hello, World!\n");
+    server();
+    return 0;
+}
